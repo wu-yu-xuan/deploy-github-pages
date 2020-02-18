8b0e289714a676f1357f3fe34259d8a933e03c9a
吴聿轩 <929003281@qq.com>
2020-02-18 19:33:10 +0800

    init branch action

--- src/git.ts
+++
@@ -1,5 +0,0 @@
-import { exec } from '@actions/exec';
-
-export default function git(...args: string[]) {
-  return exec('git', args);
-}
--- src/setGitUser.ts
+++
@@ -1,8 +0,0 @@
-import git from './git';
-
-export default async function setGitUser(userName: string, userEmail: string) {
-  return Promise.all([
-    git('config', 'user.name', userName),
-    git('config', 'user.email', userEmail)
-  ]);
-}
